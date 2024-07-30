36f0ff299368a79530b11e57d5f71b294a17964d
Onni Virtanen <onni.virtanen5@gmail.com>
2024-07-30 23:01:22 +0300

    gitignore

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 
 int main() {
-    int *p_int = (int *)heap_allocate(4);
+    int *p_int = (int *)heap_allocate(sizeof(int));
     *p_int = 42;
     printf("dereferenced p_int: %d\n", *p_int);
     printf("pointer p_int: %p\n", p_int);
