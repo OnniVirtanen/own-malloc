1387b935a2cc0ee3d1b7bb076b1b7e6da9071255
Onni Virtanen <onni.virtanen5@gmail.com>
2024-07-30 23:07:52 +0300

    deleting unused things

--- memory.c
+++ memory.c
@@ -3,14 +3,6 @@
 #include <stdbool.h>
 #include <sys/mman.h>
 
-#define HEAP_SHIFT(start) ((void *)start + sizeof(heap_t))
-#define BLOCK_SHIFT(start) ((void *)start + sizeof(heap_block_t))
-
-#define TINY_HEAP_ALLOCATION_SIZE (4 * getpagesize())
-#define TINY_BLOCK_SIZE (TINY_HEAP_ALLOCATION_SIZE / 128)
-#define SMALL_HEAP_ALLOCATION_SIZE (16 * getpagesize())
-#define SMALL_BLOCK_SIZE (SMALL_HEAP_ALLOCATION_SIZE / 128)
-
 typedef struct {
     struct heap_block_t *start;
     size_t available_size;
@@ -27,7 +19,7 @@ void *heap_allocate(size_t size) {
     if (size <= 0) {
         return NULL;
     }
-    
+    printf("size: %ld\n", size);
     void *heap = (heap_t *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 
     return heap;
